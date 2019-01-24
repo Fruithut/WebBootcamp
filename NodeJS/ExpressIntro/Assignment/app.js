e44bfb741291827f41dd89624d8c5bf8c87820b4
Fruithut <fruithut@tutanota.com>
2019-01-25 00:34:11 +0100

    Added some exercises I've done with ExpressJS

---
+++ NodeJS/ExpressIntro/Assignment/app.js
@@ -0,0 +1,33 @@
+let express = require("express");
+let app = express();
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server has started.")
+})
+
+// routes
+app.get("/", function () {
+    console.log("Hi there, welcome to my assignment!");
+})
+
+app.get("/speak/:animal", function (req, res) {
+    let animal = req.params['animal'];
+    switch (animal) {
+        case "pig": res.send("The pig says 'Oink'"); break;
+        case "cow": res.send("The cow says 'Moo'"); break;
+        case "dog": res.send("The dog says 'Woof Woof!'"); break;
+        default: res.send("This animal is mute");
+    }
+})
+
+app.get("/repeat/:word/:iter", function (req, res) {
+    let result = "";
+    for (let i = 0; i < req.params['iter']; i++) {
+        result = result + " " + req.params['word'];;
+    }
+    res.send(result);
+})
+
+app.get("*", function (req, res) {
+    res.send("Sorry, page not found.. What are you doing with your life?");
+})
---
+++ NodeJS/ExpressIntro/FirstExpressApp/app.js
@@ -0,0 +1,32 @@
+let express = require("express");
+let app = express();
+
+// let server listen
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server has started");
+});
+
+// routes
+app.get("/", function (req, res) {
+    res.send("Hi there!");
+});
+
+app.get("/bye", function (req, res) {
+    console.log("Someone made a request to /bye");
+    res.send("Goodbye!");
+})
+
+// matching route with pattern (reddit example)
+app.get("/r/:subName", function (req, res) {
+    let sub = req.params.subName;
+    res.send("WELCOME TO SUB: " + sub.toUpperCase());
+})
+
+app.get("/r/:subName/comments/:id/:title/", function (req, res) {
+    res.send("WELCOME TO THE COMMENTS");
+})
+
+// * must come last in the order of routes (matches against ever path)
+app.get("*", function (req, res) {
+    res.send("Sorry this page does not exist.")
+})
