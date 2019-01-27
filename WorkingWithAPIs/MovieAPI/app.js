875ac48b3a76126003c12b666cf197584eb149a7
Fruithut <fruithut@tutanota.com>
2019-01-27 20:26:35 +0100

    Made a very simple site for movie searches

---
+++ WorkingWithAPIs/MovieAPI/app.js
@@ -0,0 +1,26 @@
+const request = require("request");
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Movie app has started...")
+})
+
+app.get("/", function(req, res) {
+    res.render("search");
+})
+
+app.get("/results", function (req, res) {
+    let searchTerm = req.query.search;
+    request(`https://omdbapi.com/?s=${searchTerm}&apikey=thewdb`, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let results = JSON.parse(body);
+            res.render("results", {results: results});
+        }
+    })
+})
+
+
+
+
