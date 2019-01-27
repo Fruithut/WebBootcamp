519e8d9eac0a146336ee2e4290e0840b208527d1
Fruithut <fruithut@tutanota.com>
2019-01-27 02:02:20 +0100

    Some more practice with ExpressJS and EJS

---
+++ NodeJS/ExpressIntro/PostRequestDemo/app.js
@@ -0,0 +1,28 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+
+// Just some sample data -> use a database later
+let friends = ["Ole", "Dole", "Doffen", "Donald"];
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server has started.");
+})
+
+app.get("/", function (req, res) {
+    res.render("home");
+})
+
+app.get("/friends", function (req, res) {
+    res.render("friends", { friends: friends });
+})
+
+app.post("/addfriend", function (req, res) {
+    let newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+})
---
+++ NodeJS/ExpressIntro/UsingEJS/app.js
@@ -0,0 +1,30 @@
+let express = require("express");
+let app = express();
+
+// Sets express to serve public folder
+app.use(express.static("public"));
+// render files now default to ejs type
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+})
+
+app.get("/youchoose/:thing", function(req, res) {
+    let thing = req.params.thing;
+    res.render("choose", {thingVar: thing});
+})
+
+app.get("/posts", function(req, res) {
+    let posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "Post 2", author: "Charlie"},
+        {title: "Post 3", author: "Peter"},
+    ];
+
+    res.render("posts", {posts: posts});
+})
+
+app.listen(process.env.PORT || 3000, function start() {
+    console.log("Server has started.");
+})
