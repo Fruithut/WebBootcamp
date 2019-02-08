d45427f80ce3268b53dd4a7a7d7fd5ff3c9a3c12
Fruithut <fruithut@tutanota.com>
2019-02-08 11:38:25 +0100

    Added basic database functionality to yelp camp site.
    Signed-off-by: Fruithut <fruithut@tutanota.com>

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,21 +1,20 @@
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser");
+let express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-// This is going to a database soon
-let testgrounds = [
-    {name: "testCamp1", image: "https://forthemommas.com/wp-content/uploads/2015/09/campgrounds.jpg"},
-    {name: "testCamp2", image: "http://www.destination360.com/north-america/us/kentucky/images/s/kentucky-camping.jpg"},
-    {name: "testCamp3", image: "https://cbsla.files.wordpress.com/2012/05/camping-fishing-lake1.jpg"},
-    {name: "testCamp4", image: "https://forthemommas.com/wp-content/uploads/2015/09/campgrounds.jpg"},
-    {name: "testCamp5", image: "http://www.destination360.com/north-america/us/kentucky/images/s/kentucky-camping.jpg"},
-    {name: "testCamp6", image: "https://cbsla.files.wordpress.com/2012/05/camping-fishing-lake1.jpg"},
-    {name: "testCamp5", image: "http://www.destination360.com/north-america/us/kentucky/images/s/kentucky-camping.jpg"},
-    {name: "testCamp6", image: "https://cbsla.files.wordpress.com/2012/05/camping-fishing-lake1.jpg"}
-];
+// Schema setup
+let campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+let campgroundModel = mongoose.model("Campground", campgroundSchema);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("[YelpCamp] Server has started");
@@ -26,19 +25,43 @@ app.get("/", function (req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-
-    res.render("campgrounds", {campgrounds: testgrounds});
+    campgroundModel.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    })
 });
 
 app.post("/campgrounds", function (req, res) {
     let name = req.body.name;
     let image = req.body.image;
-    let newCamp = {name: name, image: image};
-    testgrounds.push(newCamp);
-    // Redirect back to campgrounds overview
+    let desc = req.body.description;
+    let newCamp = {name: name, image: image, description: desc};
+
+    campgroundModel.create(newCamp, function (err, response) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("NEWLY CREATED CAMPGROUND:");
+            console.log(response);
+        }
+    });
+
     res.redirect("/campgrounds")
 });
 
 app.get("/campgrounds/new", function (req, res) {
     res.render("new")
+});
+
+app.get("/campgrounds/:id", function (req, res) {
+    campgroundModel.findById(req.params.id, function(err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("show", {campground: result})
+        }
+    });
 });
